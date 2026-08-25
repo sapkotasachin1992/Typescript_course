7823445f4014e244da4abd7099341b5a5d5ca5ca
Sachin Sapkota <sachinsapkota1992@gmail.com>
2026-08-25 22:14:41 +0545

    index signature

---
+++ src/20_index_signatures.ts
@@ -0,0 +1,30 @@
+
+//this is used when we have to regulate key and value types
+type CounterType = { [k: string]: number }
+
+const counter: CounterType = {}
+
+counter["likes"] = 12
+counter["comments"] = 23
+counter["followers"] = 2000
+
+
+
+console.log(counter);
+
+
+//let's define more safer and tighter signatures
+//here we define the total number of keys and its value type
+type Metrices = Record<'likes' | 'views' | 'shares', number>
+
+const user: Metrices = {
+    "likes": 2999,
+    "views": 343434,
+    "shares": 10
+}
+
+
+//when we have dynamic key at runtime insted of record we can use map
+
+const price = new Map<string, number>()
+price.set('likes', 20)
